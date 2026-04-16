d495bef640539cbc09ec9d1080928fd4df021e19
maxwellthomasonpmg <maxwellthomason@pmg.com>
2026-04-16 16:13:52 -0500

    chore(passxmp): bump to 2.0.0 for UI redesign

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="passxmp",
-    version="1.0.0",
+    version="2.0.0",
     description="Mirror Lightroom presets as DaVinci Resolve .cube LUT files",
     author="Maxwell Thomason",
     url="https://github.com/maxthomason/PassXMP",
